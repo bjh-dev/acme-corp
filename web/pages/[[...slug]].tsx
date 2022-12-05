eeff58f33521211edaac2d730c7369f48f6072c9
Bryan Hickey <bjh@bjh.dev>
2022-12-06 09:46:10 +1100

    move default SEO to _app

--- web/pages/[[...slug]].tsx
+++ web/pages/[[...slug]].tsx
@@ -1,12 +1,9 @@
-import imageUrlBuilder from '@sanity/image-url'
 import groq from 'groq'
 import { GetServerSideProps, GetStaticPaths, GetStaticProps } from 'next'
-import { NextSeo } from 'next-seo'
 
 import Layout from '@/components/global/Layout'
 import RenderSections from '@/components/RenderSections'
 import { client } from '@/lib/sanity'
-import { LandingPageProps } from '@/lib/types'
 import { getSlugVariations, slugParamToPath } from '@/lib/urls'
 
 const pageFragment = groq`
@@ -109,8 +106,6 @@ export const getStaticProps: GetStaticProps = async (props) => {
     }
 }
 
-const builder = imageUrlBuilder(client)
-
 const LandingPage = ({
     title = '',
     description,
@@ -120,48 +115,13 @@ const LandingPage = ({
     openGraphImage,
     config,
     ...rest
-}: LandingPageProps) => {
-    const openGraphImages = openGraphImage
-        ? [
-              {
-                  url: builder
-                      .image(openGraphImage)
-                      .width(800)
-                      .height(600)
-                      .url(),
-                  width: 800,
-                  height: 600,
-                  alt: title,
-              },
-              {
-                  // Facebook recommended size
-                  url: builder
-                      .image(openGraphImage)
-                      .width(1200)
-                      .height(630)
-                      .url(),
-                  width: 1200,
-                  height: 630,
-                  alt: title,
-              },
-              {
-                  // Square 1:1
-                  url: builder
-                      .image(openGraphImage)
-                      .width(600)
-                      .height(600)
-                      .url(),
-                  width: 600,
-                  height: 600,
-                  alt: title,
-              },
-          ]
-        : []
-
+}) => {
     return (
-        <Layout config={config}>
-            {content && <RenderSections sections={content} />}
-        </Layout>
+        <>
+            <Layout config={config}>
+                {content && <RenderSections sections={content} />}
+            </Layout>
+        </>
     )
 }
 
